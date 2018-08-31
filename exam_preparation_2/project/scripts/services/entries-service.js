58fff9c3157798e31ef897c8addfdad9a9c52c5f
dbzahariev <dbzahariev@gmail.com>
2018-08-31 15:50:20 +0300

    Add new files

--- exam_preparation_2/project/scripts/services/entries-service.js
+++ exam_preparation_2/project/scripts/services/entries-service.js
@@ -4,19 +4,16 @@ let entriesService = (()=> {
 
         return remote.get('appdata', endPoint, 'kinvey')
     }
-
     function create(type, quantity, price, receiptId) {
         const data = {type, quantity, price, receiptId}
 
         return remote.post('appdata', 'entries', 'kinvey', data)
     }
-
     function remove(entryId) {
         const endPoint = `entries/${entryId}`
 
         return remote.remove('appdata', endPoint, 'kinvey')
     }
-
     return {
         getAllByReceiptId,
         create,
